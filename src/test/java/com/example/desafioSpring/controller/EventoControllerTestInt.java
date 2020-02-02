e4f3e985cb076d3c98fad168bde2775381ebe0c9
luiz faleiros <luizhesafa21@gmail.com>
2020-02-02 14:52:18 -0300

    Teste de data foi feito

--- src/test/java/com/example/desafioSpring/controller/EventoControllerTestInt.java
+++ src/test/java/com/example/desafioSpring/controller/EventoControllerTestInt.java
@@ -7,6 +7,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import com.example.desafioSpring.domain.dto.evento.request.EventoRequest;
+import com.example.desafioSpring.domain.dto.evento.request.EventoSearchData;
 import com.example.desafioSpring.domain.dto.evento.request.EventoUpdate;
 import com.example.desafioSpring.domain.dto.evento.response.EventoResponse;
 import com.example.desafioSpring.domain.entities.CategoriaEvento;
@@ -44,107 +45,167 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @TestPropertySource(locations = IntegrationTestConfig.appProperties)
 @ActiveProfiles("test")
 public class EventoControllerTestInt {
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper mapper;
+        @Autowired
+        private ObjectMapper mapper;
 
-    @Autowired
-    private EventoRepository eventoRepository;
+        @Autowired
+        private EventoRepository eventoRepository;
 
-    @Autowired
-    private EventoStatusRepository eventoStatusRepository;
+        @Autowired
+        private EventoStatusRepository eventoStatusRepository;
 
-    @Autowired
-    private CategoriaEventoRepository categoriaEventoRepository;
+        @Autowired
+        private CategoriaEventoRepository categoriaEventoRepository;
 
-    @Test
-    public void should_create_whenPostValid() throws JsonProcessingException, Exception {
-        Calendar ini = Calendar.getInstance();
-        ini.add(Calendar.DATE, 1);
-        Calendar fim = Calendar.getInstance();
-        fim.add(Calendar.DATE, 1);
-        fim.add(Calendar.HOUR, 2);
-        StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
-        CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome").build();
-        eventoStatusRepository.saveAndFlush(statusEvento);
-        categoriaEventoRepository.saveAndFlush(categoriaEvento);
-        EventoRequest request = EventoRequest.builder().dataHoraInicio(ini.getTime()).dataHoraFim(fim.getTime())
-                .descricao("Descricao aqui").idCategoriaEvento(1).limiteVagas(2).local("Local do evento")
-                .nome("Nome do envento").build();
+        @Test
+        public void should_create_whenPostValid() throws JsonProcessingException, Exception {
+                Calendar ini = Calendar.getInstance();
+                ini.add(Calendar.DATE, 1);
+                Calendar fim = Calendar.getInstance();
+                fim.add(Calendar.DATE, 1);
+                fim.add(Calendar.HOUR, 2);
+                StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
+                CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome")
+                                .build();
+                eventoStatusRepository.saveAndFlush(statusEvento);
+                categoriaEventoRepository.saveAndFlush(categoriaEvento);
+                EventoRequest request = EventoRequest.builder().dataHoraInicio(ini.getTime()).dataHoraFim(fim.getTime())
+                                .descricao("Descricao aqui").idCategoriaEvento(1).limiteVagas(2)
+                                .local("Local do evento").nome("Nome do envento").build();
 
-        MvcResult result = mockMvc
-                .perform(MockMvcRequestBuilders.post("/eventos").contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(request)))
-                .andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+                MvcResult result = mockMvc
+                                .perform(MockMvcRequestBuilders.post("/eventos").contentType(MediaType.APPLICATION_JSON)
+                                                .content(mapper.writeValueAsString(request)))
+                                .andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk())
+                                .andReturn();
 
-        EventoResponse response = mapper.readValue(result.getResponse().getContentAsString(), EventoResponse.class);
+                EventoResponse response = mapper.readValue(result.getResponse().getContentAsString(),
+                                EventoResponse.class);
 
-        assertNotNull(response.getIdEvento());
-    }
+                assertNotNull(response.getIdEvento());
+        }
 
-    @Test
-    public void should_getEmptyList_whenGetEmpty() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/eventos")) // Executa
-                .andDo(MockMvcResultHandlers.print()) // pega resultado
-                .andExpect(MockMvcResultMatchers.status().isOk()) // faz a validação.
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8));
-    }
+        @Test
+        public void should_getEmptyList_whenGetEmpty() throws Exception {
+                mockMvc.perform(MockMvcRequestBuilders.get("/eventos")) // Executa
+                                .andDo(MockMvcResultHandlers.print()) // pega resultado
+                                .andExpect(MockMvcResultMatchers.status().isOk()) // faz a validação.
+                                .andExpect(MockMvcResultMatchers.content()
+                                                .contentType(MediaType.APPLICATION_JSON_UTF8));
+        }
 
-    @Test
-    public void should_get_whenGetById() throws Exception {
-        StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
-        CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome").build();
-        eventoStatusRepository.saveAndFlush(statusEvento);
-        categoriaEventoRepository.saveAndFlush(categoriaEvento);
-        Evento model = Evento.builder().dataHoraFim(new Date()).dataHoraInicio(new Date())
-                .descricao("Descricao do evento").idEvento(1).limiteVagas(10).local("Local do evento")
-                .nome("Nomde do evento").categoriaEvento(categoriaEvento).eventoStatus(statusEvento).build();
-        eventoRepository.saveAndFlush(model);
+        @Test
+        public void should_get_whenGetById() throws Exception {
+                StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
+                CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome")
+                                .build();
+                eventoStatusRepository.saveAndFlush(statusEvento);
+                categoriaEventoRepository.saveAndFlush(categoriaEvento);
+                Evento model = Evento.builder().dataHoraFim(new Date()).dataHoraInicio(new Date())
+                                .descricao("Descricao do evento").idEvento(1).limiteVagas(10).local("Local do evento")
+                                .nome("Nomde do evento").categoriaEvento(categoriaEvento).eventoStatus(statusEvento)
+                                .build();
+                eventoRepository.saveAndFlush(model);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/eventos/" + model.getIdEvento())) // Executa
-                .andDo(MockMvcResultHandlers.print()) // pega resultado
-                .andExpect(MockMvcResultMatchers.status().isOk()); // faz a validação.
-    }
+                mockMvc.perform(MockMvcRequestBuilders.get("/eventos/" + model.getIdEvento())) // Executa
+                                .andDo(MockMvcResultHandlers.print()) // pega resultado
+                                .andExpect(MockMvcResultMatchers.status().isOk()); // faz a validação.
+        }
 
-    @Test
-    public void should_put_whenPutIsValid() throws Exception {
-        Calendar ini = Calendar.getInstance();
-        ini.add(Calendar.DATE, 1);
-        Calendar fim = Calendar.getInstance();
-        fim.add(Calendar.DATE, 1);
-        fim.add(Calendar.HOUR, 2);
+        @Test
+        public void should_put_whenPutIsValid() throws Exception {
+                Calendar ini = Calendar.getInstance();
+                ini.add(Calendar.DATE, 1);
+                Calendar fim = Calendar.getInstance();
+                fim.add(Calendar.DATE, 1);
+                fim.add(Calendar.HOUR, 2);
 
-        // MOCKANDO O STATUS
-        StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
-        eventoStatusRepository.saveAndFlush(statusEvento);
+                // MOCKANDO O STATUS
+                StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
+                eventoStatusRepository.saveAndFlush(statusEvento);
 
-        //MOCKANDO A CATEGORIA
-        CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome").build();
-        categoriaEventoRepository.saveAndFlush(categoriaEvento);
+                // MOCKANDO A CATEGORIA
+                CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome")
+                                .build();
+                categoriaEventoRepository.saveAndFlush(categoriaEvento);
 
-        //MOCKANDO O EVENTO
-        Evento model = Evento.builder().dataHoraFim(new Date()).dataHoraInicio(new Date())
-                .descricao("Descricao do evento").idEvento(1).limiteVagas(10).local("Local do evento")
-                .nome("Nomde do evento").categoriaEvento(categoriaEvento).eventoStatus(statusEvento).build();
-        eventoRepository.saveAndFlush(model);
+                // MOCKANDO O EVENTO
+                Evento model = Evento.builder().dataHoraFim(new Date()).dataHoraInicio(new Date())
+                                .descricao("Descricao do evento").idEvento(1).limiteVagas(10).local("Local do evento")
+                                .nome("Nomde do evento").categoriaEvento(categoriaEvento).eventoStatus(statusEvento)
+                                .build();
+                eventoRepository.saveAndFlush(model);
 
-        //CRIANDO UM REQUEST DE UPDATE
-        EventoUpdate request = EventoUpdate.builder().dataHoraInicio(ini.getTime()).dataHoraFim(fim.getTime())
-                .descricao("Descricao aqui").idCategoriaEvento(1).limiteVagas(2).local("Local do evento")
-                .nome("Nome do envento").idEventoStatus(1).build();
-                
+                // CRIANDO UM REQUEST DE UPDATE
+                EventoUpdate request = EventoUpdate.builder().dataHoraInicio(ini.getTime()).dataHoraFim(fim.getTime())
+                                .descricao("Descricao aqui").idCategoriaEvento(1).limiteVagas(2)
+                                .local("Local do evento").nome("Nome do envento").idEventoStatus(1).build();
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/eventos/"+model.getIdEvento()) 
-                .contentType(MediaType.APPLICATION_JSON) //
-                .content(mapper.writeValueAsString(request))) // Executa
-                .andDo(MockMvcResultHandlers.print()) // pega resultado
-                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+                MvcResult result = mockMvc
+                                .perform(MockMvcRequestBuilders.put("/eventos/" + model.getIdEvento())
+                                                .contentType(MediaType.APPLICATION_JSON) //
+                                                .content(mapper.writeValueAsString(request))) // Executa
+                                .andDo(MockMvcResultHandlers.print()) // pega resultado
+                                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 
-        EventoResponse response = mapper.readValue(result.getResponse().getContentAsString(), EventoResponse.class);
+                EventoResponse response = mapper.readValue(result.getResponse().getContentAsString(),
+                                EventoResponse.class);
 
-        assertNotNull(response.getIdEvento());
-    }
+                assertNotNull(response.getIdEvento());
+        }
 
+        @Test
+        public void should_list_WhenCategoriaIsValid() throws Exception {
+
+                StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
+                CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome")
+                                .build();
+                eventoStatusRepository.saveAndFlush(statusEvento);
+                categoriaEventoRepository.saveAndFlush(categoriaEvento);
+                Evento model = Evento.builder().dataHoraFim(new Date()).dataHoraInicio(new Date())
+                                .descricao("Descricao do evento").idEvento(1).limiteVagas(10).local("Local do evento")
+                                .nome("Nomde do evento").categoriaEvento(categoriaEvento).eventoStatus(statusEvento)
+                                .build();
+                eventoRepository.saveAndFlush(model);
+
+                mockMvc.perform(MockMvcRequestBuilders
+                                .get("/eventos/categoria/" + model.getCategoriaEvento().getIdCategoriaEvento())) // Executa
+                                .andDo(MockMvcResultHandlers.print()) // pega resultado
+                                .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers
+                                                .content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+        }
+
+        @Test
+        public void should_list_WhenDateIsVAlid() throws Exception {
+                Calendar ini = Calendar.getInstance();
+                Calendar fim = Calendar.getInstance();
+                fim.add(Calendar.HOUR, 4);
+
+                StatusEvento statusEvento = StatusEvento.builder().idEventoStatus(1).NomeStatus("Nome").build();
+                eventoStatusRepository.saveAndFlush(statusEvento);
+
+                CategoriaEvento categoriaEvento = CategoriaEvento.builder().idCategoriaEvento(1).nomeCategoria("nome")
+                                .build();
+                categoriaEventoRepository.saveAndFlush(categoriaEvento);
+
+                Evento model = Evento.builder().dataHoraFim(new Date()).dataHoraInicio(new Date())
+                                .descricao("Descricao do evento").idEvento(1).limiteVagas(10).local("Local do evento")
+                                .nome("Nomde do evento").categoriaEvento(categoriaEvento).eventoStatus(statusEvento)
+                                .build();
+                eventoRepository.saveAndFlush(model);
+
+                EventoSearchData request = EventoSearchData.builder().dataHoraFim(fim.getTime())
+                                .dataHoraInicio(ini.getTime()).build();
+
+                mockMvc.perform(MockMvcRequestBuilders.post("/eventos/dataSearch/")
+                                .contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(request)))
+                                .andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk())
+                                .andExpect(MockMvcResultMatchers.content()
+                                                .contentType(MediaType.APPLICATION_JSON_UTF8));
+
+        }
 }
